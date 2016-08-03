67b9aecfb41d5c762004e9e5d3bc326641ea7ea2
Mhairi McNeill <mhairihmcneill@gmail.com>
2016-08-03 19:38:19 +0100

    Styled

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     pump = require('pump'),
+    serve = require('gulp-serve'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    concatCss = require('gulp-concat-css'),
+    cleanCss = require('gulp-clean-css'),
     annotate = require('gulp-ng-annotate');
 
 var paths = {
@@ -9,6 +12,12 @@ var paths = {
         'bower_components/angular/angular.js',
         'js/**/*.js',
     ],
+    styles: [
+        'styles/**/*.css'
+    ],
+    fonts: [
+        'fonts/*'
+    ]
 };
 
 gulp.task('js', function (cb) {
@@ -29,10 +38,37 @@ gulp.task('js-min', function (cb) {
     ], cb)
 });
 
-gulp.task('watch', function (cb) {
-    gulp.watch(paths.scripts, ['js']);
+gulp.task('css', function (cb) {
+    pump([
+        gulp.src(paths.styles),
+        concatCss('app.css'),
+        gulp.dest('app/css/')
+    ], cb);
 });
 
-gulp.task('release', ['js-min']);
+gulp.task('css-min', function (cb) {
+    pump([
+        gulp.src(paths.styles),
+        concatCss('app.css'),
+        cleanCss(),
+        gulp.dest('app/css/')
+    ], cb);
+});
 
-gulp.task('default', ['js']);
+gulp.task('files', function (cb) {
+    pump([
+        gulp.src(paths.fonts),
+        gulp.dest('app/fonts/')
+    ], cb)
+})
+
+gulp.task('watch', ['serve'], function (cb) {
+    gulp.watch(paths.scripts, ['js']);
+    gulp.watch(paths.styles, ['css']);
+});
+
+gulp.task('serve', ['js', 'css', 'files'], serve('app'));
+
+gulp.task('release', ['js-min', 'css-min']);
+
+gulp.task('default', ['js', 'css']);
--- js/app.js
+++ js/app.js
@@ -11,107 +11,29 @@
         var ctrl = this;
 
         ctrl.questions = [
-            {
-                "id": 1,
-                "q": "Older workers are hard to train"
-            },
-            {
-                "id": 2,
-                "q": "Older workers do not want to train"
-            },
-            {
-                "id": 3,
-                "q": "Older workers are not economical to train"
-            },
-            {
-                "id": 4,
-                "q": "Older workers do not get good training outcomes"
-            },
-            {
-                "id": 5,
-                "q": "Older workers are too close to retirement"
-            },
-            {
-                "id": 6,
-                "q": "Older workers are too cautious"
-            },
-            {
-                "id": 7,
-                "q": "Older workers are skilled"
-            },
-            {
-                "id": 8,
-                "q": "Older workers are productive employees"
-            },
-            {
-                "id": 9,
-                "q": "Older workers cannot easily adapt to new technology"
-            },
-            {
-                "id": 10,
-                "q": "Older workers are quick to absorb new knowledge"
-            },
-            {
-                "id": 11,
-                "q": "Older workers are disruptive in team meetings"
-            },
-            {
-                "id": 12,
-                "q": "Older workers dislike taking orders from younger workers"
-            },
-            {
-                "id": 13,
-                "q": "Older workers have poor physical health and stamina"
-            },
-            {
-                "id": 14,
-                "q": "Older workers do not cope well with stress"
-            },
-            {
-                "id": 15,
-                "q": "Older workers are more likely to require time away from work"
-            },
-            {
-                "id": 16,
-                "q": "Older workers are more expensive to employ than younger workers"
-            },
-            {
-                "id": 17,
-                "q": "Older workers can't do the tasks my business needs"
-            },
-            {
-                "id": 18,
-                "q": "Older workers do not fit with my customers"
-            },
-            {
-                "id": 19,
-                "q": "Older workers have valuable experience"
-            },
-            {
-                "id": 20,
-                "q": "Older workers are not flexible"
-            },
-            {
-                "id": 21,
-                "q": "Older workers mean I would need to make adaptions to the workplace"
-            },
-            {
-                "id": 22,
-                "q": "I prefer younger workers"
-            },
-            {
-                "id": 23,
-                "q": "In a redundancy situation it is preferable to retain older workers"
-            },
-            {
-                "id": 24,
-                "q": "Older workers are more reliable than younger workers"
-            },
-            {
-                "id": 25,
-                "q": "The pace of change at work makes it difficult for older workers to cope more so than younger workers"
-            }
-        ];
+        {
+            "id": 1,
+            "q": "Older workers are hard to train"
+        },
+
+        {
+            "id": 2,
+            "q": "Older workers are too close to retirement"
+        },
+        {
+            "id": 3,
+            "q": "Older workers are too cautious"
+        },
+        {
+            "id": 4,
+            "q": "Older workers cannot easily adapt to new technology"
+        },
+        {
+            "id": 5,
+            "q": "Older workers dislike taking orders from younger workers"
+        }
+       
+         ];
 
         ctrl.currentQuestionIndex = 0;
         ctrl.question = ctrl.questions[0]; 
@@ -124,17 +46,17 @@
             {val: 5, text: 'Strongly disagree'}
         ];
 
-        ctrl.answers = [];
+        ctrl.answers = {};
 
         for (var i = 0; i < ctrl.questions.length; i++) {
-            ctrl.answers[i] = 3;
+            ctrl.answers[ctrl.questions[i].id] = 3;
         }
 
 
         ctrl.submitData = function () {
             $rootScope.answers = ctrl.answers;
 
-            ctrl.answers = [];
+            ctrl.answers = {};
         }
 
         ctrl.changeQuestion = function (change) {
@@ -159,28 +81,27 @@
 
         var ctrl = this;
         
-        var sum = $rootScope.answers.map(function (val) {
-            var num = Number(val);
-            return isNaN(num) ? 3 : num;
-        }).reduce(function (acc, val) {
-            return acc + val;
-        }) - 25;
+        var sum = 0;
+
+        angular.forEach($rootScope.answers, function (val, key) {
+            return sum += val;
+        });
 
         ctrl.result = function () {
-            if (sum <= 20) {
+            if (sum <= 5) {
                 return "love older workers";
             }
-            else if (sum <= 40) {
+            else if (sum <= 10) {
                 return "think older workers are okay";                    
             }
-            else if (sum <= 60) {
+            else if (sum <= 15) {
                 return "are neutral on older workers";                    
             }
-            else if (sum <= 80) {
+            else if (sum <= 20) {
                 return "are not keen on older workers";                    
             }
             else  {
-                return "hate older workers";    
+                return "strongly dislike older workers";    
             }
 
         }
